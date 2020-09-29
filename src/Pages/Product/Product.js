d7b5b968241d1724206ebecf1b83a73dea8f5599
laurent heneman <laurentheneman@imac-de-laurent.home>
2020-09-29 10:50:13 +0200

    Page Profile

--- src/Pages/Product/Product.js
+++ src/Pages/Product/Product.js
@@ -1,10 +1,36 @@
 import React from "react";
+import {ObjectProduct} from "../../Rooting/const";
 
-
-const Product = () => {
-
+const Product = ({match}) => {
+    const {params: id } = match;
+    console.log(match);
+    // recupérer id du produit
+    // récupérer dans le tableau les détails du produit avec l'id en utilisation filter
+    // afficher les infos
+    console.log(id.id)
+    const ShowProduct = () => {
+        const ProduitResult = ObjectProduct.map(produit => {
+            if (produit.id === (Number(id.id)) ){
+                return(
+                    <div>
+                        {produit.name},
+                        {produit.category},
+                        posté par {produit.userName}
+                        Le : {produit.datePost},
+                        {produit.prix}
+                        <img className="imageProduit" src={produit.img} alt="produit"/>
+                    </div>
+                )
+            } else { return (null)}
+        })
+        return ProduitResult
+    }
+    
+    console.table(ObjectProduct);
     return (<div>
         Produit
+        {ShowProduct()}
+        
     </div>)
 }
 
