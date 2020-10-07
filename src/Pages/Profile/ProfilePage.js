c65a5113b3851fed30679cdf569e471fc10ea0d9
Kouassi507 <yaokouassi.pro@gmail.com>
2020-10-07 13:40:26 +0200

    Mise ajour

--- src/Pages/Profile/ProfilePage.js
+++ src/Pages/Profile/ProfilePage.js
@@ -6,39 +6,53 @@ import AvatarProfile from "./AvatarProfile";
 import ButtonProfile from "./ButtonProfile";
 import ProfileName from "./ProfileName";
 
-const ProfilePage = ({ Bonjour }) => {
-  //const [cours, aller] = useState(true);
+import "./Profile.css";
 
-  //const jour = () => {
-  //aller(true);
-  //};
+
+const ProfilePage = ({ Bonjour }) => {
+ 
 
   return (
-    <div className="profileGlobal">
-      <div className="Photoprofile">
-        <AvatarProfile />
-      </div>
-      <div>
-        <ProfileName />
-      </div>
-      <div className="modifavatar">
-        <ButtonProfile text="Modifier Avatar" />
-      </div>
-      <div className="modifpassword">
-        <ButtonProfile text="Modifier mot de passe" />
-      </div>
-      <div className="mesannonces">
-        <ButtonProfile text="Mes annonces" />
-      </div>
-      <div className="creationannonces">
-        <ButtonProfile text="Créer une annonce" />
-      </div>
-      <div className="mesachats">
-        <ButtonProfile text="Mes achats" />
-      </div>
-      <div className="deconnexion">
-        <ButtonProfile text="Déconnexion" />
-      </div>
+    <div className="pageprofile">
+
+            <div className="name-photo">
+
+                    <div className="Photoprofile">
+                        <AvatarProfile />
+                    </div>
+
+                    <div class="name-profile">
+                      <ProfileName />
+                    </div>
+
+                    <div className="modifavatar">
+                        <ButtonProfile text="Modifier Avatar" />
+                      </div>
+
+                      <div className="modifpassword">
+                        <ButtonProfile text="Modifier mot de passe" />
+                      </div>
+
+              </div>
+                <div className="mesboutons">
+
+                      <div className="mesannonces">
+                          <ButtonProfile text="Mes annonces" />
+                      </div>
+
+                      <div className="creationannonces">
+                          <ButtonProfile text="Créer une annonce" />
+                        </div>
+
+
+                        <div className="mesachats">
+                            <ButtonProfile text="Mes achats" />
+                        </div>
+
+                        <div className="deconnexion">
+                          <ButtonProfile text="Déconnexion" />
+                        </div>
+              </div>
     </div>
   );
 };
