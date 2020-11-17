266e1b68a52f4ef19bf8598f0cb54f2e92bbb240
Ju'manji <67460476+MagniezJ@users.noreply.github.com>
2020-11-17 08:56:57 +0100

    1ere FETCH

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,12 +1,27 @@
-import React from "react";
+import React,{useEffect} from "react";
 import Recent from "./Recent";
 import './Home.css';
+/* import GetUser from './../../fetch.js'; */
 import chevronGauche from "../../Images/chevronGauche.png";
 import chevronDroite from "../../Images/chevronDroite.png";
 import chevronHaut from "../../Images/chevronHaut.png";
 
+const URL=new Request("http://localhost:3006/user");
 const Home = () => {
-
+    useEffect(() => {
+        fetch(URL,{ method: "GET",
+        headers: {
+            "Accept": "application/json",
+            "Access-Control-Allow-Origin":"http://localhost:3006/user"
+        }})
+        .then(response => {
+                response.json().then(json => {
+                console.log(json);
+            })
+        }
+    )
+    },[]);
+/* GetUser(); */
     return (
     <div className="homeBody">
         <div className="headBody"><h1>Bienvenue sur RetroHunt</h1></div>
