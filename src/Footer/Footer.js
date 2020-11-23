a5d6e7e9f0ae8a51daecf042b05a88c843ce31b5
Ju'manji <67460476+MagniezJ@users.noreply.github.com>
2020-11-23 16:57:01 +0100

    Footer TADAM

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -5,25 +5,34 @@ import linkedin from '../Images/linkedin.svg';
 import MentionLegale from "./MentionLegale";
 import LogoFooter from "./LogoFooter";
 import './Footer.css';
+import Col from 'react-bootstrap/Col';
+
+import Row from "react-bootstrap/Row";
 
 const Footer=()=>{
 
-    const icones=[facebook,twitter,linkedin];
+    const icones=[facebook,linkedin];
 
     const afficheur=icones.map((any, index)=>
     <div className="Footer" key={index}><img src={any} alt="réseaux sociaux"/></div>);
     return (
-    <div className="FooterGlobal">
-
-       <div className="Afficheur">
-           {afficheur}
-        </div>
-
-       <div className="LogoMentions">
-            <LogoFooter/>
-            <MentionLegale/>
-       </div>
-
+        <div className="d-flex flex-column">
+        <footer className="footer" >
+            <Row className="p">
+                <Col xs={6}>
+                    <h5 className="Mentions mx-auto d-block">Site crée par Zackary, Benoit et Justine</h5>
+                </Col>
+                <Col>
+                <MentionLegale/>
+                </Col>
+                <Col>
+                <img className="Footer" src={facebook} alt="réseaux sociaux"></img>
+                {/* {afficheur} */}
+                
+                <img className="Footer" src={linkedin} alt="réseaux sociaux"></img>
+                </Col>
+            </Row>
+    </footer>
     </div>
     )
 }
