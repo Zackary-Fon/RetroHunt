12131c2d76e174a8c1086f59b6f9772e117c303f
Benoît Fournier <benoitfournier28pro@gmail.com>
2020-11-24 13:16:12 +0100

    header modifs

--- src/Header/Burger.js
+++ src/Header/Burger.js
@@ -1,28 +1,28 @@
-import React, { useState } from 'react';
-import './Header.css';
-import menu from '../Images/menu.png';
-import list from './ListMenu';
+// import React, { useState } from 'react';
+// import './Header.css';
+// import menu from '../Images/menu.png';
+// import list from './ListMenu';
 
-const Burger = () =>{
+// const Burger = () =>{
 
-    /* déclaration du state qui sert a afficher le menu lorsque l'on clique sur le burger*/ 
-    const [open, setOpen] = useState(false);
-    /*const test = useRef();
-    console.log("test", test.current);
-    useLayoutEffect(() => {
-        console.log("test", test.current);
-      return () => {
-      },
-      []
-    })*/
+//     /* déclaration du state qui sert a afficher le menu lorsque l'on clique sur le burger*/ 
+//     const [open, setOpen] = useState(false);
+//     /*const test = useRef();
+//     console.log("test", test.current);
+//     useLayoutEffect(() => {
+//         console.log("test", test.current);
+//       return () => {
+//       },
+//       []
+//     })*/
 
-    return(
-        <>
-        <img /*ref={test}*/ src={menu} alt="menu" className="burger" onClick={()=> {setOpen(!open)}}/>
-         {/*Si open vaut true alors on affiche le menu qui est celui de la version desktop */}
-        {open? <ul className="burgerMenu">{list()}</ul>:null}
-        </>
-    )
+//     return(
+//         <>
+//         <img /*ref={test}*/ src={menu} alt="menu" className="burger" onClick={()=> {setOpen(!open)}}/>
+//          {/*Si open vaut true alors on affiche le menu qui est celui de la version desktop */}
+//         {open? <ul className="burgerMenu">{list()}</ul>:null}
+//         </>
+//     )
 
-}
-export default Burger;
+// }
+// export default Burger;
--- src/Header/ListMenu.js
+++ src/Header/ListMenu.js
@@ -1,59 +1,30 @@
 import React, { useState } from 'react';
-import {Link} from "react-router-dom";
 import tabRoutes from "../Rooting/const";
-import BigMenu from "./BigMenu";
+import Nav from "react-bootstrap/Nav";
+import NavDropdown from "react-bootstrap/NavDropdown";
+// import BigMenu from "./BigMenu";
+import Searchbar from "./Searchbar";
+import BigMenuDropdown from "./BigMenuDropdown"
 
 //création de la liste,à partir du tableau de routes qui se situe dans "const" ,qui sera le menu
 
 const list = () =>{
-    const [bigMenuDrop, setDropdown] = useState(false);
-    const [click, setClick] = useState(false);
-    const handleClick = () => setClick(!click);
-    const closeMobileMenu = () => setClick(false);
-
-    const onMouseEnter = () => {
-        if(window.innerWidth < 960) {
-            setDropdown(false);
-        }else {
-            setDropdown(true);
-        }
-    }
-
-    const onMouseLeave = () => {
-        if(window.innerWidth < 960) {
-            setDropdown(false);
-        }else {
-            setDropdown(false);
-        }
-    }
-
     const result = tabRoutes.map((any, index)=>{
-        // Ici on affiche catalogue sur l'id égal à 1
-    if (index == 1){
-        return  (    
-        <>   
-            <li className="li_menu_head"
-                onMouseEnter={onMouseEnter}
-                onMouseLeave={onMouseLeave}
-            >
-                <Link>
-                    Catalogue <i className="fas fa-caret-down"></i>
-                </Link>
-                {bigMenuDrop && <BigMenu/>}
-            </li>
-        </>
-        )
-    }
-
         /*on met un if car l'index 0 est la route de l'accueil et on met ce lien sur le logo
         du site et les index supérieurs à 5 sont les routes vers d'autres pages comme 
         la mention légale etc... qui ne seront pas dans ce menu */
-
-    if (index>1 && index<5){
+    if (index==1){
         return (
-            <li className="li_menu_head" key={any.id}>
-                <Link to={any.path}>{any.name}</Link>
-            </li>
+            <>
+                    <Nav.Link key={any.id} href={any.path}>{any.name}</Nav.Link>
+                    <NavDropdown title="" id="basic-nav-dropdown">
+                        <BigMenuDropdown/>
+                    </NavDropdown>
+            </>
+        )
+    } if (index>1 && index<5){
+        return (
+                <Nav.Link key={any.id} href={any.path}>{any.name}</Nav.Link>
         )
     } else {
         return null
