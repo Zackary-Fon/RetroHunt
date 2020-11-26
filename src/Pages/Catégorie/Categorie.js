dc079ec81e288cef89fa51eebc47ce2963e3b4b6
Zackary-Fon <66824618+Zackary-Fon@users.noreply.github.com>
2020-11-26 11:19:30 +0100

    profil

--- src/Pages/Catégorie/Categorie.js
+++ src/Pages/Catégorie/Categorie.js
@@ -1,15 +1,11 @@
 import React from "react";
 import {ObjectProduct} from "../../Rooting/const";
-import tabRoutes from "../../Rooting/const";
 
 const Categorie = ({match}) => {
     // recupérer category du produit
     const {params: category } = match;
     console.log(match);
     const ShowCategorie = () => {
-        // récupérer dans le tableau les détails du produit avec un if pour correspondre à l'id
-        //du produit sur lequel on a cliqué puis on affiche les informations et l'image dans une card
-        // et on affiche la description que l'utilisateur à écrit sur une deuxième card
         const categorieResult = ObjectProduct.map((produit) => {
             if (produit.category === (String(name.name)) ){
                 return(
--- src/Rooting/const.js
+++ src/Rooting/const.js
@@ -2,7 +2,7 @@ import ConnexionInscription from "../Pages/ConnexionInscription/ConnexionInscrip
 import Home from "../Pages/Home/Home";
 import Legal from "../Pages/Legal/Legal";
 import Product from "../Pages/Product/Product";
-import Profile from "../Pages/Profile/Profile";
+import Profil from "../Pages/Profil/Profil";
 import Search from "../Pages/Search/Search";
 import Categorie from "../Pages/Catégorie/Categorie";
 import Crash_Bandicoot1 from "./ImagesProduits/Crash_Bandicoot1.jpg";
@@ -19,7 +19,7 @@ const tabRoutes = [
 {id:0, name:"Home", path:"/", component:Home},
 {id:1, name:"Catégories", path:"/Catalogue", component:Categorie},
 {id:2, name:"Connexion/inscription", path:"/ConnexionInscription", component:ConnexionInscription},
-{id:3, name:"Profil", path:"/Profile", component:Profile},
+{id:3, name:"Profil", path:"/Profil", component:Profil},
 {id:4, name:"Déposer une annonce", path:"/DeposerAnnonce", component:AjouterAnnonce},
 {id:5, name:"Product", path:"/Product/:id", component:Product},
 {id:6, name:"Search", path:"/Search", component:Search},
