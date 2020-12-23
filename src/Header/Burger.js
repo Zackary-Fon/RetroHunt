5d83e7d92d953e0512ba702be84eb164ad64ffde
Ju'manji <67460476+MagniezJ@users.noreply.github.com>
2020-12-23 10:08:22 +0100

    casse les couilles

--- src/Header/Burger.js
+++ src/Header/Burger.js
@@ -2,16 +2,27 @@
 
 
 const Burger = () =>{
+    
+const local=sessionStorage;
+const connexionB=document.querySelector('#connexion2');
+console.log(connexionB)
+const profilB=document.querySelector('#profil2');
+console.log(profilB)
+const decoB=document.querySelector('#deco2');
     const burger=document.querySelector('.burger');
     burger.addEventListener('click',()=>{
         console.log('click')
         const menuB=document.querySelector('.menuB');
         const searchbutton=document.querySelector('.burgersearch');
+        
+console.log(local)
         if(menuB.style.display === "none"){
             menuB.style.display="block";
+           
         }else{
             menuB.style.display="none";
         }
+
         const Jeux=document.getElementById('Jeux');
 
         Jeux.addEventListener('click',()=>{
@@ -22,6 +33,17 @@ const Burger = () =>{
         })
     })
     
+    if (local.length >=1 && local.Token != "undefined"){
+        console.log('connecter')
+        profilB.style.display="block !important";
+        connexionB.style.display="none !important";
+        decoB.style.display="block !important";
+    }else{
+        console.log('pas co ')
+        profilB.style.display="none !important";
+        connexionB.style.display="block !important";
+        decoB.style.display="none !important";
+    }
 
 }
 export default Burger;
