063c059f3ee11688cd608f211fddbf75937a3f6b
Ju'manji <67460476+MagniezJ@users.noreply.github.com>
2020-12-29 10:12:29 +0100

    card dans mes annonces

--- src/Pages/Chat/nouvelleConv.js
+++ src/Pages/Chat/nouvelleConv.js
@@ -40,6 +40,47 @@ console.log('ok')
 
 )
 })
+
+window.addEventListener("keydown", (eve)=> {
+    if (eve.key=="Enter"  ){
+    const ladate= new Date();
+    const date=ladate.getDate()+"/"+(ladate.getMonth()+1)+"/"+ladate.getFullYear()
+    const Conv={
+        envoyeur:document.getElementById('envoyeur').value,
+        receveur:document.getElementById('Receveur').value,
+        Titre:document.getElementById('titre').innerHTML,
+        IdAnn:document.getElementById('idAnn').value,
+        Message:[{message:document.getElementById('message').value,aEnvoye:document.getElementById('envoyeur').value,
+    Date:date}]
+    }
+    const jsConv=JSON.stringify(Conv);
+    
+const conf={ method: "POST",
+headers: {
+"Accept": "application/json",
+"Content-Type": "application/json",
+"Access-Control-Allow-Origin":"http://localhost:3006/message/new"
+},
+body:jsConv};
+fetch("http://localhost:3006/message/new",conf)
+.then(response => response.json().then((json) => {
+console.log(json)
+if(json ==="deja une conv"){
+console.log('mess')
+
+document.location.href='/Chat'+document.getElementById('idAnn').value;
+
+
+}
+else{
+document.location.href='/Chat/'+document.getElementById('idAnn').value;
+console.log('ok')
+
+} 
+})
+
+)}
+})
 }
 
 export default convers
